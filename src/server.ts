a826e187de50936b6b367618c3321ede317d0d2c
pratiksingh001 <singhpratik0001@gmail.com>
2026-03-15 00:00:00 +0530

    checking the lint staged setup

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
 function greet(name: string) {
   console.log('Hello ' + name);
+
+  const user = {
+    name: 'Pratik',
+  };
+
+  const fName = user['name'];
+  return name + ' ' + fName;
 }
 
 greet('Pratik');
