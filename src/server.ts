44980aa140e87a86fcddfe9dfe08cc0f064da49e
pratiksingh001 <singhpratik0001@gmail.com>
2026-03-15 00:16:42 +0530

    finished git hooks

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,5 @@
 function greet(name: string) {
   console.log('Hello ' + name);
-
-  const user = {
-    name: 'Pratik',
-  };
-
-  const fName = user['name'];
-  return name + ' ' + fName;
 }
 
 greet('Pratik');
